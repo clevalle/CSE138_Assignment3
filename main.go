682f6c8471cda14a6fdd6265e4b661f2247ab560
Dennis Pang <dpang321@gmail.com>
2021-11-17 18:46:29 -0800

    updated

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
+// standard imports
 import (
 	"bytes"
 	"encoding/json"
@@ -15,47 +16,50 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// message struct is used to unpack request vals into a struct that can handle null causal metadata
 type message struct {
 	Value          interface{}  `json:"value"`
 	CausalMetadata *ReqMetaData `json:"causal-metadata"`
 }
 
+// reqMetaData is used to unpack request vals when they actually exist and are not null so they can be easily assigned a type
 type ReqMetaData struct {
 	ReqVector       [3]int `json:"ReqVector, omitempty"`
 	ReqIpIndex      int    `json:"ReqIpIndex, omitempty"`
 	IsReqFromClient bool   `json:"IsReqFromClient, omitempty"`
 }
 
+// declaring our Vector Clock, which we'll use for causal consistency
 type VectorClock struct {
 	VC [3]int `json:"VC, omitempty"`
 }
 
-var replicaArray []string
-var replicaCount = 0
-var sAddress string
-var viewArray []string
-var vectorIndex = -1
-
-const downReplicaPath = "/"
-const aliveCheckConst = true
+var replicaArray []string // holds IP's of all replicas
+var replicaCount = 0      // local Counter for number of replicas online
+var sAddress string       // socket address
+var viewArray []string    // array of IP's currently in view i.e. online
+var vectorIndex = -1      // represents which index in replicaArray this current thread is
 
 // first 3 integers represent the vector clock of the local replica
 // 4th vector is the index of the Ip that all replicas have access to
 // 5th vector is the binary toggle (1 or 0) that determines if the data was from a client (1) or a replica (0)
 var localVector = [3]int{0, 0, 0}
 
+// Used in mapping replica IP to index of the replicaArray
 var ipToIndex = map[string]int{
 	"10.10.0.2:8090": 0,
 	"10.10.0.3:8090": 1,
 	"10.10.0.4:8090": 2,
 }
 
-var indexToIp = map[int]string{
-	0: "10.10.0.2:8090",
-	1: "10.10.0.3:8090",
-	2: "10.10.0.4:8090",
-}
+// Used in mapping index to replica IP of replicaArray
+// var indexToIp = map[int]string{
+// 	0: "10.10.0.2:8090",
+// 	1: "10.10.0.3:8090",
+// 	2: "10.10.0.4:8090",
+// }
 
+// our local KVS store
 var store = make(map[string]interface{})
 
 func main() {
@@ -92,20 +96,26 @@ func main() {
 	r.HandleFunc("/getVC", handleGetVC)
 	r.HandleFunc("/getKVS", handleGetKVS)
 
+	// function that checks if this replica has just died
 	go didIDie()
-
-	//go aliveCheck()
 
 	// Service listens on port 8090
 	log.Fatal(http.ListenAndServe(":8090", r))
 }
 
+// Used to check if current replica has just died
 func didIDie() {
+	// sleep for a second, so that we can confirm
 	time.Sleep(time.Second * 1)
+	// checking all elements of current view
 	for _, replicaIP := range viewArray {
+		// if any in the view is not our address
 		if replicaIP != sAddress {
+			// gets vector clock of that other replica
 			var repVC = getReplicaVectorClock(replicaIP)
 			fmt.Println("repVC === ", repVC)
+
+			// if other replica's VC is not equal to our own
 			if repVC != localVector {
 				//set local VC to grabbed VC
 				localVector = repVC
@@ -118,18 +128,24 @@ func didIDie() {
 	}
 }
 
+// Function used to send our IP to a replica's view array
 func pushIpToReplicas(replicaIP string) {
+	// making a response, and map our socket address
 	response := make(map[string]string)
 	response["socket-address"] = sAddress
 
+	// standard json marshal of repsonse
 	jsonResponse, err := json.Marshal(response)
 	if err != nil {
 		log.Fatalf("Error here: %s", err)
 	}
+
+	// checking each replica IP of all replicas
 	for _, replicaIP := range replicaArray {
+		// if the replica IP is not our own
 		if replicaIP != sAddress {
 			client := &http.Client{}
-			// Creating new request
+			// Creating new request to PUT our IP in the replica's view array
 			req, err := http.NewRequest("PUT", fmt.Sprintf("http://%s/view", replicaIP), bytes.NewBuffer(jsonResponse))
 			if err != nil {
 				fmt.Println("problem creating new http request")
@@ -147,6 +163,7 @@ func pushIpToReplicas(replicaIP string) {
 
 }
 
+// Function used to get the kvs store of another replica
 func getReplicaKVS(replicaIP string) map[string]interface{} {
 	var response map[string]interface{}
 
@@ -156,14 +173,17 @@ func getReplicaKVS(replicaIP string) map[string]interface{} {
 		fmt.Println("problem creating new http request")
 	}
 
+	// decoding the response of new request
 	decodeError := json.NewDecoder(res.Body).Decode(&response)
 	if decodeError != nil {
 		log.Fatalf("Error: %s", err)
 	}
 
+	// returning the KVS
 	return response["KVS"].(map[string]interface{})
 }
 
+// Function used  tog et the vector clock of another replica
 func getReplicaVectorClock(replicaIP string) [3]int {
 	var response VectorClock
 
@@ -175,11 +195,13 @@ func getReplicaVectorClock(replicaIP string) [3]int {
 		log.Fatalf("Error: %s", err)
 	}
 
+	// decoding the response of new request
 	decodeError := json.NewDecoder(res.Body).Decode(&response)
 	if decodeError != nil {
 		log.Fatalf("Error: %s", err)
 	}
 
+	// returning the VC from other replica
 	return response.VC
 }
 
@@ -223,6 +245,7 @@ func isAlive(replicaIP string) bool {
 }
 */
 
+// Helper function used to check if the database has been changed
 func isDatabaseChanged(response map[string]interface{}) bool {
 
 	//check if a value actually got added to db, and if so we need to alert the other replicas
@@ -235,6 +258,7 @@ func isDatabaseChanged(response map[string]interface{}) bool {
 	return false
 }
 
+// Helper function used to broadcast a message to a replica
 func broadcastMessage(replicaIP string, req *http.Request, updatedBody []byte) {
 
 	client := &http.Client{}
@@ -248,29 +272,46 @@ func broadcastMessage(replicaIP string, req *http.Request, updatedBody []byte) {
 		return
 	}
 
+	// Checking to see if we can even reach that IP
 	reachedURL, err := net.DialTimeout("tcp", replicaIP, (1 * time.Second))
+	// If we can't reach that Replica, we know that it is down
+	// Therefore, we must then broadcast and update views of all replicas
 	if err != nil {
 		fmt.Println(replicaIP, " is down! didnt reply within 2 seconds")
+
+		// finding the index of the replica in array of online replicas
 		i := containsVal(replicaIP, replicaArray)
 		if i >= 0 {
+			// removing that index from the array of online replicas
 			replicaArray = removeVal(i, replicaArray)
 		}
 		fmt.Println("view is now ===", replicaArray)
+
+		// Looping thru all replica IPs in replicaArray
 		for _, repIP := range replicaArray {
+			// if that replica IP is not the one we are broadcasting to, and is not our current replica
 			if repIP != replicaIP && repIP != sAddress {
 				fmt.Println("tell ", repIP, "that ", replicaIP, " is down")
+
+				// making a variable to hold replicaIP
 				viewBody := map[string]string{
 					"socket-address": replicaIP,
 				}
+
+				// standard json marshalling
 				viewBodyJson, err := json.Marshal(viewBody)
 				if err != nil {
 					log.Fatalf("Error: %s", err)
 				}
+
+				// checking if repIP is reachable
 				reachedURL, err := net.DialTimeout("tcp", repIP, (2 * time.Second))
 				if err != nil {
 					fmt.Println("couldnt reach for DELETE", repIP, "err ===", err)
 					return
 				}
+
+				// Creating new delete request to deletethe down replica IP from a replica's view
 				reachedURL.Close()
 				viewReq, err := http.NewRequest("DELETE", fmt.Sprintf("http://%s/view", repIP), bytes.NewBuffer(viewBodyJson))
 				if err != nil {
@@ -278,6 +319,7 @@ func broadcastMessage(replicaIP string, req *http.Request, updatedBody []byte) {
 					return
 				}
 
+				// Sending delete request to the replica
 				res, err := client.Do(viewReq)
 				if err != nil {
 					fmt.Println("problem creating new http request for view delete broadcast")
@@ -302,15 +344,16 @@ func broadcastMessage(replicaIP string, req *http.Request, updatedBody []byte) {
 	return
 }
 
-func inReplicaArray(addr string) bool {
-	for _, viewIP := range replicaArray {
-		if viewIP == addr {
-			return true
-		}
-	}
-	return false
-}
+// func inReplicaArray(addr string) bool {
+// 	for _, viewIP := range replicaArray {
+// 		if viewIP == addr {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
 
+// Helper function to check if an array contains a certain value, and at what index
 func containsVal(val string, repArray []string) int {
 	for index, a := range repArray {
 		if a == val {
@@ -320,11 +363,14 @@ func containsVal(val string, repArray []string) int {
 	return -1
 }
 
+// Helper function to remove a Value from a certain string array
 func removeVal(index int, repArray []string) []string {
 	repArray[index] = repArray[len(repArray)-1]
 	return repArray[:len(repArray)-1]
 }
 
+// Handler Function that handles when we are given a request to return
+// our local VC, which we send out as a JSON object
 func handleGetVC(w http.ResponseWriter, req *http.Request) {
 	response := make(map[string]interface{})
 
@@ -339,6 +385,8 @@ func handleGetVC(w http.ResponseWriter, req *http.Request) {
 	w.Write(jsonResponse)
 }
 
+// Handler functuon that handles when we are guven a request to return
+// our local KVS, which we send out as json object
 func handleGetKVS(w http.ResponseWriter, req *http.Request) {
 	response := make(map[string]interface{})
 
@@ -353,16 +401,15 @@ func handleGetKVS(w http.ResponseWriter, req *http.Request) {
 	w.Write(jsonResponse)
 }
 
+// Handler function that handles all operations wheb we are  given
+// requests regarding our KVS
 func handleKey(w http.ResponseWriter, req *http.Request) {
 
 	// grabbing params to be used
 	param := mux.Vars(req)
 	key := param["key"]
 
-	if len(key) > 0 {
-
-	}
-
+	// initilizations of necessary variables
 	response := make(map[string]interface{})
 	broadcastResponse := make(map[string]interface{})
 	var responseMetadata ReqMetaData
@@ -377,10 +424,12 @@ func handleKey(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	// assigning metadata from our request
 	metadata := reqVals.CausalMetadata
 
 	fmt.Println("localvector on recieve === ", localVector)
 
+	// If metadata is not empty, we  know that this is not first interaction with client
 	if metadata != nil {
 		reqVector := metadata.ReqVector
 		responseMetadata.IsReqFromClient = metadata.IsReqFromClient
@@ -487,11 +536,13 @@ func handleKey(w http.ResponseWriter, req *http.Request) {
 			}
 		}
 
+		// reassigning necessary values in our response metadata
 		responseMetadata.ReqVector = localVector
 		responseMetadata.ReqIpIndex = ipToIndex[sAddress]
 
+		// checking if we changed our database, and if so, to increment VC
 		if isDatabaseChanged(response) {
-
+			// Incrementing if the request is from the client
 			if responseMetadata.IsReqFromClient {
 				localVector[vectorIndex]++
 			}
@@ -540,6 +591,7 @@ func handleKey(w http.ResponseWriter, req *http.Request) {
 
 }
 
+// Handler function that handles all program behavior regarding view operations
 func handleView(w http.ResponseWriter, req *http.Request) {
 
 	response := make(map[string]interface{})
@@ -557,12 +609,14 @@ func handleView(w http.ResponseWriter, req *http.Request) {
 		}
 		val := newVal["socket-address"]
 
+		// auto adding if this is first replica
 		if replicaCount == 0 {
 			replicaArray = append(replicaArray, val)
 			w.WriteHeader(http.StatusCreated)
 			response["result"] = "added"
 			replicaCount++
 		} else {
+			// checking to make sure entry is already  present
 			if containsVal(val, replicaArray) >= 0 {
 				w.WriteHeader(http.StatusOK)
 				response["result"] = "already present"
@@ -573,14 +627,12 @@ func handleView(w http.ResponseWriter, req *http.Request) {
 				replicaCount++
 			}
 		}
-
 	} else if req.Method == "GET" {
-
+		// simply returning replica array for view
 		w.WriteHeader(http.StatusOK)
 		response["view"] = replicaArray
 
 	} else if req.Method == "DELETE" {
-
 		// handles pulling out and storing value into newVal
 		err := json.NewDecoder(req.Body).Decode(&newVal)
 		if err != nil {
@@ -589,13 +641,17 @@ func handleView(w http.ResponseWriter, req *http.Request) {
 		}
 		val := newVal["socket-address"]
 
+		// finding index of the value in replica array
 		index := containsVal(val, replicaArray)
+		// if it is found
 		if index >= 0 {
+			// delete the replica from view
 			replicaArray = removeVal(index, replicaArray)
 			w.WriteHeader(http.StatusCreated)
 			response["result"] = "deleted"
 			replicaCount--
 		} else {
+			// returning when replica is not found
 			w.WriteHeader(http.StatusNotFound)
 			response["error"] = "View has no such replica"
 		}
@@ -610,6 +666,8 @@ func handleView(w http.ResponseWriter, req *http.Request) {
 
 }
 
+// Handler  function to handle program behavior when we need to restore
+// a replica after it has been down
 func handleDown(w http.ResponseWriter, req *http.Request) {
 	// This function is passed in a "flag" parameter
 	// If the flag is set to 0 -- We must send the kvs store as a response
